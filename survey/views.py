3dd4152f0136e456d81004e7290e4eee6362689a
andamuthu <andamuthu.a@gmail.com>
2020-09-05 14:06:06 +0530

    second commit

--- survey/views.py
+++ survey/views.py
@@ -3,7 +3,7 @@ from survey.forms import *
 from survey.models import *
 
 
-def survey(request):
+def survey(request):----
     if request.method == 'GET':
         form = SurveyForm()
         return render(request, 'survey.html', {'surveyform': form})
